e449735f6b2bd46b604c896593d93e5a9d70b4ec
flaj5477 <flaj5477@daum.net>
2019-10-24 17:48:14 +0900

    session에서 memberNo가져오기 적용

--- DevDeveloper/src/main/java/com/dd/devdeveloper/projects/controller/ProjectsController.java
+++ DevDeveloper/src/main/java/com/dd/devdeveloper/projects/controller/ProjectsController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.dd.devdeveloper.members.MembersVO;
 import com.dd.devdeveloper.projects.ProjParticipantsVO;
 import com.dd.devdeveloper.projects.ProjectsVO;
 import com.dd.devdeveloper.projects.service.ProjectsService;
@@ -38,7 +39,8 @@ public class ProjectsController {
 	@RequestMapping("/applyProjects")
 	public String applyProjects(ProjParticipantsVO vo, HttpSession session) {
 		//vo에 지원자 no 담기
-		vo.setMembersNo(2); /////////////////////////////////////membersId말고 members객체 넣을꺼임 바꿔야함!
+		//vo.setMembersNo(2); /////////////////////////////////////membersId말고 members객체 넣을꺼임 바꿔야함!
+		vo.setMembersNo(  ((MembersVO)session.getAttribute("members")).getMembersNo() );
 		vo.setProjNo((Integer)session.getAttribute("projNo"));
 		projectsService.applyProjects(vo);
 		return "redirect:/projectsList";	//프로젝트 목록 페이지로 돌아감

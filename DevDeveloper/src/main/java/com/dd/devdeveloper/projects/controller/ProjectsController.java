84d5d381a131ea4faed1af13c5892b47cc5aae6d
flaj5477 <flaj5477@daum.net>
2019-11-01 12:24:47 +0900

    WikiTransVO, paging 임포트

--- DevDeveloper/src/main/java/com/dd/devdeveloper/projects/controller/ProjectsController.java
+++ DevDeveloper/src/main/java/com/dd/devdeveloper/projects/controller/ProjectsController.java
@@ -1,6 +1,5 @@
 package com.dd.devdeveloper.projects.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.dd.devdeveloper.common.paging.Paging;
 import com.dd.devdeveloper.members.MembersVO;
 import com.dd.devdeveloper.projects.ProjParticipantsVO;
 import com.dd.devdeveloper.projects.ProjectsVO;
